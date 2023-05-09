ed7fcf5b034cbe5da37446989d78220a7158bdab
Joost Lubach <joostlubach@gmail.com>
2023-05-09 12:02:06 +0200

    Removed ignored dist stuff

--- dist/util.d.ts
+++
@@ -1,3 +0,0 @@
-import { LayoutRect, Size } from './types';
-export declare function layoutRectEquals(rect1: LayoutRect, rect2: LayoutRect): boolean;
-export declare function sizeEquals(size1: Size, size2: Size): boolean;
